9e6fe2e24e96c4cddbdabccc3b05efdb08e4ddba
Alejandro <alexblascochafer16@gmail.com>
2024-11-25 13:42:17 +0100

    lk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.t6ejercicio1"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.t6ejercicio1"
